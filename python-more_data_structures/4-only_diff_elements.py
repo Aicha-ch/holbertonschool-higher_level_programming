ecf9f0989193f173e941acf003d1c6fa7e6b1908
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-10-05 03:49:26 -0700

    pycodestyle correction

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -6,4 +6,4 @@ def only_diff_elements(set_1, set_2):
             if i not in set_2 and j not in set_1:
                 new_set.add(j)
                 new_set.add(i)
-    return(new_set)
+    return (new_set)
