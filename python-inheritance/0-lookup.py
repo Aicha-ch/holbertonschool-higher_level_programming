54d0e92e9b84db408f2e0d7058eed7b2a81f99d6
Aicha-ch <chwikhi.aycha@gmail.com>
2023-10-20 05:17:19 -0700

    revise pycode style errors

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-    This script defines a function that retrieves the list of attributes and methods available in a given object.
+    retrieve the list of attributes and methods available in a given object.
 """
 
 
 def lookup(obj):
     """
-    Function to obtain the list of attributes and methods of a specified object.
+    obtain the list of attributes and methods of a specified object.
     """
     return dir(obj)
