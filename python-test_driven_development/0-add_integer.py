b229546998c27fd6a42d7c16b96926c9ec471fd3
Aicha-ch <chwikhi.aycha@gmail.com>
2023-10-22 11:48:28 -0700

    revise code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,9 +3,13 @@
 
 
 def add_integer(a, b=98):
+    """define add function"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer or float")
-    
-    return int(a) + int(b)
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
