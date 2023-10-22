e0da7445323e49fa080735b9855d33a102775ef7
Aicha-ch <chwikhi.aycha@gmail.com>
2023-10-21 23:02:37 -0700

    revise code

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import json
 """returning the JSON representation of an object"""
+
+import json
 
 
 def to_json_string(my_obj):
+    """define json obj"""
     json_str = json.dumps(my_obj)
     return json_str
-
