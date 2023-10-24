9c3278cc4f385d21fbe7245498ae728fefcdf633
Aicha-ch <chwikhi.aycha@gmail.com>
2023-10-24 09:33:27 -0700

    import module

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-""" Create a class """
+""" Module containing class rectangle """
 
 
-class Base:
-    """ base class """
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from models.base import Base
 
 
 class Rectangle(Base):
     """ Rectangle class inherits from Base class """
+
 
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
