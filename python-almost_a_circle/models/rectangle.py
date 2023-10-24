3183c1c61c62a9eed5e1a4409a42a3594baf4ef3
Aicha-ch <chwikhi.aycha@gmail.com>
2023-10-24 09:55:23 -0700

    setter and getter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """ Module containing class rectangle """
-
-
 from models.base import Base
 
 
@@ -9,8 +7,49 @@ class Rectangle(Base):
     """ Rectangle class inherits from Base class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes a Rectangle instance"""
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """ Getter for width """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ Setter for width """
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter for height """
+        self.__height = value
+
+    @property
+    def x(self):
+        """ Getter for x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Setter for x """
+        self.__x = value
+
+    @property
+    def y(self):
+        """ Getter for y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Setter for y """
+        self.__y = value
