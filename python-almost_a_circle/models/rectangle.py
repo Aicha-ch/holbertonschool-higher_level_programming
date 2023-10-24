9143c9ac79741e50bb64a102c2ee0a7a7642de4e
Aicha-ch <chwikhi.aycha@gmail.com>
2023-10-24 09:24:08 -0700

    create a child class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,18 +19,6 @@ class Rectangle(Base):
     """ Rectangle class inherits from Base class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        Constructor.
-
-        Args:
-        width: Width of the rectangle.
-        height: Height of the rectangle.
-        x: X-coordinate of the rectangle's position.
-        y: Y-coordinate of the rectangle's position.
-        id:optional identifier.
-
-        constructor of the parent cls(Base)called using super().__init__(id).
-        """
         self.__width = width
         self.__height = height
         self.__x = x
