5f71772cf4d798cfe8636ebeabe1334cc59479a9
Aicha-ch <chwikhi.aycha@gmail.com>
2023-10-25 09:03:25 -0700

    update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,7 +91,7 @@ class Rectangle(Base):
         return ("[Rectangle] ({}) {}/{} - {}/{}".format(
                 self.id, self.x, self.y, self.width, self.height))
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """define method"""
         if args:
             if len(args) >= 1:
@@ -104,3 +104,6 @@ class Rectangle(Base):
                 self.x = args[3]
             if len(args) >= 5:
                 self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
