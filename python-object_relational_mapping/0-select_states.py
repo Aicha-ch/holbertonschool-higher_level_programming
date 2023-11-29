907a3775f0af00e852a585e4594ddc32a7907498
Aicha-ch <chwikhi.aycha@gmail.com>
2023-11-29 11:59:47 +0100

    update

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-''' Script that lists all states from the database hbtn_0e_0_usa '''
-import MySQLdb
+"""Lists all states from the database hbtn_0e_0_usa
+"""
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-
-    ''' Connect to MySQL server running locally on port 3306 '''
+    username: str = sys.argv[1]
+    password: str = sys.argv[2]
+    db_name: str = sys.argv[3]
+    host: str = "localhost"
+    port: int = 3306
+    statement: str = """SELECT * FROM states ORDER BY id"""
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        host=host,
+        port=port,
+        password=password,
+        database=db_name,
     )
-
-    ''' Execute an SQL query to fetch all states '''
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cursor.execute(statement)
+    rows = cursor.fetchall()
+    for row in rows:
+        print("({}, '{}')".format(row[0], row[1]))
 
-    ''' Print the results in the desired format '''
-    for state in cursor.fetchall():
-        print(state)
-
-    ''' Close the cursor and the connection to the database '''
     cursor.close()
     db.close()
