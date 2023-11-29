c883062abcd60c3f28dec8d484056b5d189cf31f
Aicha-ch <chwikhi.aycha@gmail.com>
2023-11-29 11:33:15 +0100

    add docstrings

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,13 +6,15 @@ import MySQLdb
 
 if __name__ == "__main__":
 
-    ''' Connect to MySQL server running locally on port 3306 '''
+    ''' variables declaration '''
     username: str = sys.argv[1]
     password: str = sys.argv[2]
     db_name: str = sys.argv[3]
     host: str = "localhost"
     port: int = 3306
     statement: str = """SELECT * FROM states ORDER BY id"""
+
+    ''' Connect to MySQL server running locally on port 3306 '''
     db = MySQLdb.connect(
         user=username,
         host=host,
@@ -20,11 +22,16 @@ if __name__ == "__main__":
         password=password,
         database=db_name,
     )
+
+    ''' Execute an SQL query to fetch all states '''
     cursor = db.cursor()
     cursor.execute(statement)
     rows = cursor.fetchall()
+
+    ''' Print the results in the desired format '''
     for row in rows:
         print(row)
 
+    ''' Close the cursor and the connection to the database '''
     cursor.close()
     db.close()
